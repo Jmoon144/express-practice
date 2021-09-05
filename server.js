dfc360ada01a1ba2e1af510804a4be01d9330a67
박준영 <sjwm98@gmail.com>
2021-09-05 22:01:38 +0900

    Fix:delete 수정

--- server.js
+++ server.js
@@ -6,22 +6,6 @@ app.use(bodyParser.urlencoded({extended : true}));
 const MongoClient = require('mongodb').MongoClient;
 app.set('view engine', 'ejs');
 
-
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html')
-});
-
-app.get('/write', function(req, res){
-    res.sendFile(__dirname + '/write.html')
-});
-
-app.get('/list', function(req, res){
-
-    db.collection('post').find().toArray(function(err, result){
-        res.render('list.ejs', { posts : result });
-    });
-});
-
 var db;
 MongoClient.connect('mongodb+srv://mongodb:mongodb@cluster0.n1atu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', function(error, client){
 
@@ -38,28 +22,44 @@ MongoClient.connect('mongodb+srv://mongodb:mongodb@cluster0.n1atu.mongodb.net/my
     });
 
     app.post('/add', function(req, res){
+        
         res.send('전송완료');
+        console.log(req.body)
         // db.counter내의 총게시물갯수를 찾음
-        db.collection('counter').findOne({name : '게시물갯수'}, function(err, result){
+        db.collection('counter').findOne({name : '게시물갯수'}, function(err, data){
+            console.log(data.totalPost)
             // 총게시물갯수 변수에 저장
-            var 총게시물갯수 = result.totalPost;
+            var 총게시물갯수 = data.totalPost;
 
             db.collection('post').insertOne({ _id : 총게시물갯수 + 1, 제목 : req.body.title, 날짜 : req.body.date})
-            });
+            
             //counter라는 콜렉션에 있는 totalPost 라는 항목도 1 증가시켜줘야함 (수정);
-            db.collection('counter').updateOne({ name : '게시물갯수'},{ $inc: { totalPost : 1 }},function(err, result){
-                if(err){return console.log(err)}
+                db.collection('counter').updateOne({ name : '게시물갯수'},{ $inc: { totalPost : 1 }},function(err, data){
+                    if(err){return console.log(err)}
+                });
             });
         });    
+}); 
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html')
+});
+
+app.get('/write', function(req, res){
+    res.sendFile(__dirname + '/write.html')
+});
+
+app.get('/list', function(req, res){
+    
+    db.collection('post').find().toArray(function(err, data){
+        res.render('list.ejs', { posts : data });
+    });
 });
 
 app.delete('/delete', function(req, res){
-    //id값 int로 바꿔주는 js문법(parseInt)
-    req.body._id = parseInt(req.body._id)
-    //요청.body에 담겨온 게시물번호를 가진 글을 db에서 찾아서 삭제
-    db.collection('post').deleteOne(req.body, function(err, result){
-        //성공했을시 어떻게 할지 해주는 콜백함수
-        console.log('삭제완료');
-    });
-    res.send('삭제완료')
+    console.log(req.body)
+    req.body._id = parseInt(req.body._id);
+    db.collection('post').deleteOne(req.body, function(err, data){
+        console.log('삭제완료')
+    })
 })
