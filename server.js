79d371f6fad7c8bee2db7d31466c4da45b3edeb3
박준영 <sjwm98@gmail.com>
2021-09-06 15:08:13 +0900

    Fix:html, css 수정

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended : true}));
-
 const MongoClient = require('mongodb').MongoClient;
 app.set('view engine', 'ejs');
+
+app.set('/public', express.static('public'));
 
 var db;
 MongoClient.connect('mongodb+srv://mongodb:mongodb@cluster0.n1atu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', function(error, client){
@@ -26,15 +27,15 @@ MongoClient.connect('mongodb+srv://mongodb:mongodb@cluster0.n1atu.mongodb.net/my
         res.send('전송완료');
         console.log(req.body)
         // db.counter내의 총게시물갯수를 찾음
-        db.collection('counter').findOne({name : '게시물갯수'}, function(err, data){
-            console.log(data.totalPost)
+        db.collection('counter').findOne({name : '게시물갯수'}, function(err, result){
+            console.log(result.totalPost)
             // 총게시물갯수 변수에 저장
-            var 총게시물갯수 = data.totalPost;
+            var 총게시물갯수 = result.totalPost;
 
             db.collection('post').insertOne({ _id : 총게시물갯수 + 1, 제목 : req.body.title, 날짜 : req.body.date})
             
             //counter라는 콜렉션에 있는 totalPost 라는 항목도 1 증가시켜줘야함 (수정);
-                db.collection('counter').updateOne({ name : '게시물갯수'},{ $inc: { totalPost : 1 }},function(err, data){
+                db.collection('counter').updateOne({ name : '게시물갯수'},{ $inc: { totalPost : 1 }},function(err, result){
                     if(err){return console.log(err)}
                 });
             });
@@ -42,24 +43,35 @@ MongoClient.connect('mongodb+srv://mongodb:mongodb@cluster0.n1atu.mongodb.net/my
 }); 
 
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html')
+    // res.sendFile(__dirname + 'views/index.ejs')
+    res.render('index.ejs');
 });
 
 app.get('/write', function(req, res){
-    res.sendFile(__dirname + '/write.html')
+    // res.sendFile(__dirname + '/write.ejs')
+    res.render('write.ejs');
 });
 
 app.get('/list', function(req, res){
     
-    db.collection('post').find().toArray(function(err, data){
-        res.render('list.ejs', { posts : data });
+    db.collection('post').find().toArray(function(err, result){
+        res.render('list.ejs', { posts : result });
     });
 });
 
 app.delete('/delete', function(req, res){
     console.log(req.body)
     req.body._id = parseInt(req.body._id);
-    db.collection('post').deleteOne(req.body, function(err, data){
-        console.log('삭제완료')
+    db.collection('post').deleteOne(req.body, function(err, result){
+        console.log('삭제완료');
+        res.status(200).send({ Message : 'SUCCESS'});
+    });
+});
+
+app.get('/detail/:id', function(req, res){
+    db.collection('post').findOne({_id : parseInt(req.params.id)}, function(err, result){
+        console.log(result);
+        res.render('detail.ejs', { data : result });
+        
     })
 })
